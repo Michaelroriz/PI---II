ade523c3a4e02fa8ab07c4cfd865d69a41da4664
monique.ralves <monique.ralves@CAS555557W0704.senacsp.edu.br>
2017-10-16 20:02:20 -0200

    classe produto implementada

---
+++ src/br/senac/sp/model/Produto.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.sp.model;
+
+/**
+ *
+ * @author monique.ralves
+ */
+public class Produto {
+    
+    private Integer codigo;
+    private String nome;
+    private String categoria;
+    private String descricao;
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+}
