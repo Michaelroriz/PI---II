4785ae620ef92e0c1275aa3dc88cc1e321174990
monique.ralves <monique.ralves@CAS555557W0704.senacsp.edu.br>
2017-10-16 20:02:55 -0200

    alteração de pasta

---
+++ src/br/senac/sp/model/Cliente.java
@@ -0,0 +1,109 @@
+package br.senac.sp.model;
+
+import java.util.Date;
+
+//Classe de negócio de cliente
+public class Cliente {
+
+    //Atributos
+    private Integer id;
+    private String nome;
+    private String sobrenome;
+    private Date dataNascimento;
+    private String genero;
+    private String endereco;
+    private String cpf;
+    private String complemento;
+    private String cep;
+    private String cidade;
+    private String estado;
+
+    //Métodos de acesso
+    public Integer getId() {
+        return id;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero; 
+    }
+}
