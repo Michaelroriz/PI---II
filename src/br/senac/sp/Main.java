7268defcd3bea4d4f5d06459ef6824ab48d553a7
Michael <Michael@Michael-D>
2017-11-04 19:23:03 -0200

    inicializador

--- src/br/senac/sp/Main.java
+++ src/br/senac/sp/Main.java
@@ -5,6 +5,8 @@
  */
 package br.senac.sp;
 
+import br.senac.sp.ui.Menu;
+
 /**
  *
  * @author michael.aroriz
@@ -15,7 +17,8 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Menu menu = new Menu();        
+        menu.setVisible(true);
     }
     
 }
